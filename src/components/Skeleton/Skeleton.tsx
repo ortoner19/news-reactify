0d1669bde070b21d19423ab15a3f46261f1fdec5
ortoner19 <ortoner@ukr.net>
2024-01-27 19:38:41 +0200

    fix r9

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -13,7 +13,7 @@ const Skeleton = ({ count = 1, type = 'banner', direction = 'column' }: Props) =
             {count > 1 ? (
                 <ul className={direction === 'column' ? styles.columnList : styles.rowList}>
                     {[...Array(count)].map((_, index) => {
-                        <li key={index} className={type === "banner" ? styles.banner : styles.item}></li>
+                       return <li key={index} className={type === "banner" ? styles.banner : styles.item}></li>
                     })}
                 </ul>
             ) : <li className={type === "banner" ? styles.banner : styles.item}></li>}
